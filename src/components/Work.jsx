0cdc03783a5a50e366a4e87582a07f9650c21050
parth kambli <parthkambli.pk1@gmail.com>
2026-05-21 14:53:57 +0530

    changes

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -3,10 +3,18 @@ import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 import service1 from "../assets/images/service-1.png";
-import service6 from "../assets/images/service-6.png";
-import service7 from "../assets/images/service-7.png";
-import service8 from "../assets/images/service-8.png";
-import service9 from "../assets/images/service-9.png";
+import work1  from "../assets/work/1.jpg";
+import work2  from "../assets/work/2.jpg";
+import work3  from "../assets/work/3.jpg";
+import work4  from "../assets/work/4.jpg";
+import work5  from "../assets/work/5.jpg";
+import work6  from "../assets/work/6.jpg";
+import work7  from "../assets/work/7.jpg";
+import work8  from "../assets/work/8.jpg";
+import work9  from "../assets/work/9.jpg";
+import work10 from "../assets/work/10.jpg";
+import work11 from "../assets/work/11.jpg";
+import work12 from "../assets/work/12.jpg";
 
 gsap.registerPlugin(ScrollTrigger);
 
@@ -17,21 +25,21 @@ function Work() {
 
   useEffect(() => {
     gsap.to(workImagesRef.current, {
-      y: "-280vh",
+      y: "-690vh",
       ease: "none",
       scrollTrigger: {
         trigger: workSectionRef.current,
         start: "top top",
-        end: "+=3200",
+        end: "+=8600",
         scrub: true,
         pin: true,
         onUpdate: (self) => {
           if (self.progress < 0.33) {
-            setWorkWord("content");
+            setWorkWord("CONTENT");
           } else if (self.progress < 0.66) {
             setWorkWord("WEBSITE");
           } else {
-            setWorkWord("Performance");
+            setWorkWord("PERFORMANCE");
           }
         },
       },
@@ -81,30 +89,78 @@ function Work() {
 
       <div
         ref={workImagesRef}
-        className="pointer-events-none absolute left-0 top-[100vh] z-[100] h-[380vh] w-full"
+        className="pointer-events-none absolute left-0 top-[100vh] z-[100] h-[760vh] w-full"
       >
-        <img
-          src={service6}
+       <img
+          src={work1}
           alt=""
           className="absolute left-[2%] top-[0vh] w-[180px] border-[6px] border-white object-cover md:w-[270px]"
         />
 
         <img
-          src={service7}
+          src={work2}
           alt=""
-          className="absolute right-[4%] top-[75vh] w-[180px] border-[6px] border-white object-cover md:w-[270px]"
+          className="absolute right-[4%] top-[55vh] w-[180px] border-[6px] border-white object-cover md:w-[270px]"
         />
 
         <img
-          src={service8}
+          src={work3}
           alt=""
-          className="absolute right-[18%] top-[150vh] w-[180px] border-[6px] border-white object-cover md:w-[270px]"
+          className="absolute left-[15%] top-[110vh] w-[180px] border-[6px] border-white object-cover md:w-[270px]"
         />
 
         <img
-          src={service9}
+          src={work4}
           alt=""
-          className="absolute left-[8%] top-[230vh] w-[180px] border-[6px] border-white object-cover md:w-[270px]"
+          className="absolute right-[12%] top-[165vh] w-[180px] border-[6px] border-white object-cover md:w-[270px]"
+        />
+
+        <img
+          src={work5}
+          alt=""
+          className="absolute left-[6%] top-[220vh] w-[180px] border-[6px] border-white object-cover md:w-[270px]"
+        />
+
+        <img
+          src={work6}
+          alt=""
+          className="absolute right-[18%] top-[275vh] w-[180px] border-[6px] border-white object-cover md:w-[270px]"
+        />
+
+        <img
+          src={work7}
+          alt=""
+          className="absolute left-[20%] top-[330vh] w-[180px] border-[6px] border-white object-cover md:w-[270px]"
+        />
+
+        <img
+          src={work8}
+          alt=""
+          className="absolute right-[8%] top-[385vh] w-[180px] border-[6px] border-white object-cover md:w-[270px]"
+        />
+
+        <img
+          src={work9}
+          alt=""
+          className="absolute left-[10%] top-[440vh] w-[180px] border-[6px] border-white object-cover md:w-[270px]"
+        />
+
+        <img
+          src={work10}
+          alt=""
+          className="absolute right-[5%] top-[495vh] w-[180px] border-[6px] border-white object-cover md:w-[270px]"
+        />
+
+        <img
+          src={work11}
+          alt=""
+          className="absolute left-[18%] top-[550vh] w-[180px] border-[6px] border-white object-cover md:w-[270px]"
+        />
+
+        <img
+          src={work12}
+          alt=""
+          className="absolute right-[15%] top-[605vh] w-[180px] border-[6px] border-white object-cover md:w-[270px]"
         />
       </div>
     </section>
